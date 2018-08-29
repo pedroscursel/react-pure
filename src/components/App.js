8af41fa0875ff44bd5cfe21b406452f4c637bced
Pedro Scursel <pedroscursel@gmail.com>
2018-08-29 12:26:56 -0300

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component} form 'react';
+
+import '../src/styles/App.css'
+
+class App extends Comment {
+    render() {
+        return(
+            <div>
+                <h1>React App!</h1>
+            </div>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from '../scr/components/App'
+
+ReactDom.render(<App />, document.getElementByID('root'))
